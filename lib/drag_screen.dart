dc750b3c219252a40487acbdc689b4adc53ad604
unknown <saromal543@gmail.com>
2025-01-27 12:23:39 +0530

    dfg

--- lib/drag_screen.dart
+++ lib/drag_screen.dart
@@ -9,26 +9,14 @@ class HoverScaleIcon extends StatefulWidget {
 }
 
 class _HoverScaleIconState extends State<HoverScaleIcon> {
-  final List<bool> _isHovered = [false, false, false, false, false];
-  final List<IconData> icon = [
+  final List<IconData> icons = [
     Icons.person,
     Icons.message,
     Icons.call,
     Icons.camera,
     Icons.photo,
   ];
-
-  List<String> imagepath = [
-    "assets/images.jpg",
-    "assets/images.jpg",
-    "assets/images.jpg",
-    "assets/images.jpg",
-    "assets/images.jpg",
-    "assets/images.jpg",
-    "assets/images.jpg",
-    "assets/images.jpg",
-    "assets/images.jpg"
-  ];
+  final List<bool> _isHovered = [false, false, false, false, false];
 
   @override
   Widget build(BuildContext context) {
@@ -37,61 +25,32 @@ class _HoverScaleIconState extends State<HoverScaleIcon> {
       child: Container(
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(20),
-          color: const Color.fromARGB(81, 228, 82, 244),
+          color: const Color.fromARGB(53, 228, 82, 244),
         ),
-        height: 600, // Increased height for better image visibility
-        child: ReorderableGridView.count(
+        height: 100,
+        child: ReorderableGridView.builder(
+          itemCount: icons.length,
           onReorder: (oldIndex, newIndex) {
             setState(() {
-              String path = imagepath
-                  .removeAt(oldIndex); // Use setState to trigger UI update
-              imagepath.insert(newIndex, path); // Update the order of images
+              final icon = icons.removeAt(oldIndex);
+              icons.insert(newIndex, icon);
+              _isHovered[oldIndex] = false;
             });
           },
-          crossAxisCount: 3,
-          childAspectRatio: 1.5,
-          children: imagepath
-              .map((String path) => ListView.builder(
-                      key: ValueKey(path),
-                      itemBuilder: (context, index) {
-                        return MouseRegion(
-                            onEnter: (_) => setState(() {
-                                  _isHovered[index] = true;
-                                }),
-                            onExit: (_) => setState(() {
-                                  _isHovered[index] = false;
-                                }),
-                            child: AnimatedContainer(
-                              key: ValueKey(index),
-                              duration: const Duration(milliseconds: 100),
-                              transform: Matrix4.identity()
-                                ..scale(_isHovered[index] ? 1.2 : 1.0),
-                              child: Card(
-                                key: ValueKey(path),
-                                child: Image.asset(path),
-                              ),
-                            ));
-                      })
-                  //  Card(
-                  //       key: ValueKey(path),
-                  //       child: Image.asset(path),
-                  //     )
-                  )
-              .toList(),
-        ),
-      ),
-    );
-  }
-}
-
-
-
-/*
-List.generate(icon.length, (index) {
-            key:
-            ValueKey(index);
-
+          dragWidgetBuilder: (index, child) {
+            return Transform.scale(
+              scale: 1.2,
+              child: child,
+            );
+          },
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 5,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+          itemBuilder: (context, index) {
             return MouseRegion(
+              key: ValueKey(icons[index]),
               onEnter: (_) => setState(() {
                 _isHovered[index] = true;
               }),
@@ -99,23 +58,29 @@ List.generate(icon.length, (index) {
                 _isHovered[index] = false;
               }),
               child: AnimatedContainer(
-                key: ValueKey(index),
                 duration: const Duration(milliseconds: 100),
                 transform: Matrix4.identity()
                   ..scale(_isHovered[index] ? 1.2 : 1.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: const Color.fromARGB(255, 255, 255, 255),
-                  ),
-                  width: 50,
-                  height: 50,
-                  child: Icon(
-                    icon[index],
-                    size: 35,
+                child: Padding(
+                  padding: const EdgeInsets.all(25),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: const Color.fromARGB(250, 229, 229, 229),
+                    ),
+                    height: 50,
+                    width: 50,
+                    child: Icon(
+                      icons[index],
+                      size: 20,
+                    ),
                   ),
                 ),
               ),
             );
-          }
-*/
+          },
+        ),
+      ),
+    );
+  }
+}
